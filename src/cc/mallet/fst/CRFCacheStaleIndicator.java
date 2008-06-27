f979a4871c8f9c0e5c95448b1a0ba25acb30b129
Gaurav Chandalia <gsc [ a t ] cs [ d o t ] umass [ d o t ] edu>
2008-06-27 17:25:42 -0400

    Adding support for multi-threading to ValueGradient optimizables.

---
+++ src/cc/mallet/fst/CRFCacheStaleIndicator.java
@@ -0,0 +1,45 @@
+package cc.mallet.fst;
+
+
+/**
+ * Indicates when the value/gradient becomes stale based on updates to CRF's
+ * parameters.
+ *
+ * @author Gaurav Chandalia
+ */
+public class CRFCacheStaleIndicator implements CacheStaleIndicator {
+  protected CRF crf;
+
+  protected int cachedValueChangeStamp = -1;
+  protected int cachedGradientChangeStamp = -1;
+
+  public CRFCacheStaleIndicator(CRF crf) {
+    this.crf = crf;
+    cachedValueChangeStamp = -1;
+    cachedGradientChangeStamp = -1;
+  }
+
+  /**
+   * Returns true if the value is stale, also updates the cacheValueStamp.
+   */
+  public boolean isValueStale() {
+    if (crf.weightsValueChangeStamp != cachedValueChangeStamp) {
+      cachedValueChangeStamp = crf.weightsValueChangeStamp;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Returns true if the gradient is stale, also updates the cacheGradientStamp.
+   */
+  public boolean isGradientStale() {
+    if (crf.weightsValueChangeStamp != cachedGradientChangeStamp) {
+      cachedGradientChangeStamp = crf.weightsValueChangeStamp;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
