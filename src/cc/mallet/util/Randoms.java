603fc484056ed54af741f2d3a2e1f4351675b978
David Mimno <mimno@cs.umass.edu>
2009-09-10 14:32:51 -0400

    constant time sampling of gamma variates with alpha greater than 1.0

--- src/cc/mallet/util/Randoms.java
+++ src/cc/mallet/util/Randoms.java
@@ -189,38 +189,91 @@ public class Randoms extends java.util.Random {
     return nextGamma(alpha,beta,0);
   }
 
-  /** Return a random double drawn from a Gamma distribution with mean alpha*beta+lamba and variance alpha*beta^2. */
-  public synchronized double nextGamma(double alpha,double beta,double lambda) {
-    double gamma=0;
-    if (alpha <= 0 || beta <= 0) {
-      throw new IllegalArgumentException ("alpha and beta must be strictly positive.");
-    }
-    if (alpha < 1) {
-      double b,p;
-      boolean flag=false;
-      b=1+alpha*Math.exp(-1);
-      while(!flag) {
-        p=b*nextUniform();
-        if (p>1) {
-          gamma=-Math.log((b-p)/alpha);
-          if (nextUniform()<=Math.pow(gamma,alpha-1)) flag=true;
-        }
-        else {
-          gamma=Math.pow(p,1/alpha);
-          if (nextUniform()<=Math.exp(-gamma)) flag=true;
-        }
-      }
-    }
-    else if (alpha == 1) {
-      gamma = -Math.log (nextUniform ());
-    } else {
-      double y = -Math.log (nextUniform ());
-      while (nextUniform () > Math.pow (y * Math.exp (1 - y), alpha - 1))
-        y = -Math.log (nextUniform ());
-      gamma = alpha * y;
-    }
-    return beta*gamma+lambda;
-  }
+	/** Return a random double drawn from a Gamma distribution
+	 *  with mean alpha*beta+lamba and variance alpha*beta^2.
+	 *  Note that this means the pdf is: 
+	 *     <code>frac{ x^{alpha-1} exp(-x/beta) }{ beta^alpha Gamma(alpha) }</code>
+	 *  in other words, beta is a "scale" parameter. An alternative 
+	 *  parameterization would use 1/beta, the "rate" parameter.
+	 */
+	public synchronized double nextGamma(double alpha, double beta, double lambda) {
+		double gamma=0;
+		if (alpha <= 0 || beta <= 0) {
+			throw new IllegalArgumentException ("alpha and beta must be strictly positive.");
+		}
+		if (alpha < 1) {
+			double b,p;
+			boolean flag = false;
+
+			b = 1 + alpha * Math.exp(-1);
+
+			while (!flag) {
+				p = b * nextUniform();
+				if (p > 1) {
+					gamma = -Math.log((b - p) / alpha);
+					if (nextUniform() <= Math.pow(gamma, alpha - 1)) {
+						flag = true;
+					}
+				}
+				else {
+					gamma = Math.pow(p, 1.0/alpha);
+					if (nextUniform() <= Math.exp(-gamma)) {
+						flag = true;
+					}
+				}
+			}
+		}
+		else if (alpha == 1) {
+			// Gamma(1) is equivalent to Exponential(1). We can 
+			//  sample from an exponential by inverting the CDF:
+
+			gamma = -Math.log (nextUniform ());
+
+			// There is no known closed form for Gamma(alpha != 1)...
+		}
+		else {
+			
+			// This is Best's algorithm: see pg 410 of
+			//  Luc Devroye's "non-uniform random variate generation"
+			// This algorithm is constant time for alpha > 1.
+			
+			double b = alpha - 1;
+			double c = 3 * alpha - 0.75;
+			
+			double u, v;
+			double w, y, z;
+			
+			boolean accept = false;
+			
+			while (! accept) {
+				u = nextUniform();
+				v = nextUniform();
+				
+				w = u * (1 - u);
+				y = Math.sqrt( c / w ) * (u - 0.5);
+				gamma = b + y;
+				
+				if (gamma >= 0.0) {
+					z = 64 * w * w * w * v * v;  // ie: 64 * w^3 v^2
+					
+					accept = z <= 1.0 - ((2 * y * y) / gamma);
+					
+					if (! accept) {
+						accept = (Math.log(z) <=
+								  2 * (b * Math.log(gamma / b) - y));
+					}
+				}
+			}
+			
+			/* // Old version, uses time linear in alpha
+			   double y = -Math.log (nextUniform ());
+			   while (nextUniform () > Math.pow (y * Math.exp (1 - y), alpha - 1))
+			   y = -Math.log (nextUniform ());
+			   gamma = alpha * y;
+			*/
+		}
+		return beta*gamma+lambda;
+	}
 
 	/** Return a random double drawn from an Exponential distribution with mean 1 and variance 1. */
   public synchronized double nextExp() {
