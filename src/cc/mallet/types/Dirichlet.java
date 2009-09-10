401498b7a2cfd31f403ec750b5a8950b81ce1942
David Mimno <mimno@cs.umass.edu>
2009-09-10 14:31:58 -0400

    trigamma function

--- src/cc/mallet/types/Dirichlet.java
+++ src/cc/mallet/types/Dirichlet.java
@@ -377,6 +377,36 @@ Bernoulli numbers. */
 		return sum;
 	}
 
+	public static double trigamma(double z) {
+		int shift = 0;
+        while (z < 2) {
+            z++;
+            shift++;
+        }
+		
+		double oneOverZ = 1.0 / z;
+		double oneOverZSquared = oneOverZ * oneOverZ;
+
+		double result = 
+			oneOverZ +
+			0.5 * oneOverZSquared +
+			0.1666667 * oneOverZSquared * oneOverZ -
+			0.03333333 * oneOverZSquared * oneOverZSquared * oneOverZ +
+			0.02380952 * oneOverZSquared * oneOverZSquared * oneOverZSquared * oneOverZ -
+			0.03333333 * oneOverZSquared * oneOverZSquared * oneOverZSquared * oneOverZSquared * oneOverZ;
+			
+		System.out.println(z + " -> " + result);
+		
+		while (shift > 0) {
+			shift--;
+			z--;
+			result += 1.0 / (z * z);
+			System.out.println(z + " -> " + result);
+		}
+
+		return result;
+	}
+
 
 	/** 
 	 * Learn Dirichlet parameters using frequency histograms
