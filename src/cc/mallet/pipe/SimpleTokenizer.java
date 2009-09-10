315168acc993fb9dcb69ec14fcc1282bbed3d192
David Mimno <mimno@cs.umass.edu>
2009-09-10 14:45:50 -0400

    better handling of punctuation and marks (useful for Indic scripts)

--- src/cc/mallet/pipe/SimpleTokenizer.java
+++ src/cc/mallet/pipe/SimpleTokenizer.java
@@ -105,18 +105,52 @@ public class SimpleTokenizer extends Pipe {
 			for (int i=0; i < totalCodePoints; i++) {
 
 				int codePoint = Character.codePointAt(characters, i);
+				int codePointType = Character.getType(codePoint);
 
-				if (Character.isLetter(codePoint)) {
+				if (codePointType == Character.LOWERCASE_LETTER ||
+					codePointType == Character.UPPERCASE_LETTER) {
 					length++;
 					tokenBuffer[length] = codePoint;
 				}
-				else if (length != -1) {
-					String token = new String(tokenBuffer, 0, length + 1);
-					if (! stoplist.contains(token)) {
-						tokens.add(token);
+				else if (codePointType == Character.SPACE_SEPARATOR ||
+						 codePointType == Character.LINE_SEPARATOR ||
+						 codePointType == Character.PARAGRAPH_SEPARATOR ||
+						 codePointType == Character.END_PUNCTUATION ||
+						 codePointType == Character.DASH_PUNCTUATION ||
+						 codePointType == Character.CONNECTOR_PUNCTUATION ||
+						 codePointType == Character.START_PUNCTUATION ||
+						 codePointType == Character.INITIAL_QUOTE_PUNCTUATION ||
+						 codePointType == Character.FINAL_QUOTE_PUNCTUATION ||
+						 codePointType == Character.OTHER_PUNCTUATION) {
+					
+					// Things that delimit words
+					if (length != -1) {
+						String token = new String(tokenBuffer, 0, length + 1);
+						if (! stoplist.contains(token)) {
+							tokens.add(token);
+						}
+						length = -1;
 					}
-					length = -1;
 				}
+				else if (codePointType == Character.COMBINING_SPACING_MARK ||
+						 codePointType == Character.ENCLOSING_MARK ||
+						 codePointType == Character.NON_SPACING_MARK ||
+						 codePointType == Character.TITLECASE_LETTER ||
+						 codePointType == Character.MODIFIER_LETTER ||
+						 codePointType == Character.OTHER_LETTER) {
+					// Obscure things that are technically part of words.
+					//  Marks are especially useful for Indic scripts.
+
+					length++;
+					tokenBuffer[length] = codePoint;
+				}
+				else {
+					// Character.DECIMAL_DIGIT_NUMBER
+					// Character.CONTROL
+					// Character.MATH_SYMBOL
+					//System.out.println("type " + codePointType);
+				}
+					
 			}
 
 			if (length != -1) {
