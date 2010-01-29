32bd72f6c98023bfbad06d64bb4a736036b4d5f1
Kedar Bellare <kedarb@cs.umass.edu>
2010-01-28 23:25:04 -0500

    bug fix: valueToString for integer array option

--- src/cc/mallet/util/CommandOption.java
+++ src/cc/mallet/util/CommandOption.java
@@ -465,7 +465,7 @@ public abstract class CommandOption
 		public java.lang.String valueToString() {
 			StringBuffer b = new StringBuffer();
 			java.lang.String sep = "";
-			for (int i = 0; i < defaultValue.length; i++) {
+			for (int i = 0; i < value.length; i++) {
 				b.append(sep).append(java.lang.Integer.toString(value[i]));
 				sep = ",";
 			}
