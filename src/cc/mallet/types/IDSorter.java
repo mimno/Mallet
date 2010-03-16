c132ebb2f835d8e05156dc3d45fa954d2c8eea7e
David Mimno <mimno@cs.umass.edu>
2010-03-16 10:16:47 -0400

    ties now broken by ID, descending

--- src/cc/mallet/types/IDSorter.java
+++ src/cc/mallet/types/IDSorter.java
@@ -11,7 +11,7 @@ package cc.mallet.types;
  *   This class is contains a comparator for use in sorting 
  *   integers that have associated floating point values. One
  *   example would be sorting words by probability in a Naive Bayes
- *   model.
+ *   model. Ties are broken by the ID.
  *   <code><pre>
  *     IDSorter[] sortedIDs = new IDSorter[n];
  *     for (int i=0; i&lt;n; i++) {
@@ -29,15 +29,30 @@ public class IDSorter implements Comparable {
     int id; double p;
     public IDSorter (int id, double p) { this.id = id; this.p = p; }
     public IDSorter (int id, int p) { this.id = id; this.p = p; }
+
     public final int compareTo (Object o2) {
-	if (p > ((IDSorter) o2).p)
-	    return -1;
-	else if (p == ((IDSorter) o2).p)
-	    return 0;
-	else return 1;
-    }
-    public int getID() {return id;}
-    public double getWeight() {return p;}
-    /** Reinitialize an IDSorter */
-    public void set(int id, double p) { this.id = id; this.p = p; }
+
+		double otherP = ((IDSorter) o2).p;
+
+		if (p > ((IDSorter) o2).p) {
+			return -1;
+		}
+		else if (p < ((IDSorter) o2).p) {
+			return 1;
+		}
+
+		// p == otherP, sort by ID
+		
+		int otherID = ((IDSorter) o2).id;
+		
+		if (id > otherID) { return -1; }
+		else if (id < otherID) { return 1; }
+				 
+		return 0;
+	}
+	public int getID() {return id;}
+	public double getWeight() {return p;}
+
+	/** Reinitialize an IDSorter */
+	public void set(int id, double p) { this.id = id; this.p = p; }
 }
