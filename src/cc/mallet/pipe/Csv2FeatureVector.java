338c3cbe26f7491f7c2aaca590c2b2f79f385390
David Mimno <mimno@cs.umass.edu>
2010-07-15 22:13:54 -0400

    fixed bug that could have shuffled feature values, removed check for target, added default value 1.0 for features with no explicit value

--- src/cc/mallet/pipe/Csv2FeatureVector.java
+++ src/cc/mallet/pipe/Csv2FeatureVector.java
@@ -24,65 +24,48 @@ import cc.mallet.util.MalletLogger;
  * <tt>feature_1:val_1 feature_2:val_2 ... feature_k:val_k</tt>
  * into a (sparse) FeatureVector.
  *
+ * Features with no ":" character are assumed to have value 1.0.
+ * 
  * @author Gary Huang
  */
-public class Csv2FeatureVector extends Pipe 
-{
+public class Csv2FeatureVector extends Pipe {
 
     private static Logger logger = MalletLogger.getLogger(Csv2FeatureVector.class.getName());
 
-    public Csv2FeatureVector(int capacity) 
-    {
+    public Csv2FeatureVector(int capacity) {
         this.dataAlphabet = new Alphabet(capacity);
     }
     
-    public Csv2FeatureVector() 
-    {
+    public Csv2FeatureVector() {
         this(1000);
     }
     
     /**
      * Convert the data in the given <tt>Instance</tt> from a <tt>CharSequence</tt> 
      * of sparse feature-value pairs to a <tt>FeatureVector</tt>
-     *
-     * @throws IllegalStateException If <CODE>Instance.getTarget()</CODE> is
-     * not a Labeling
      */
-    public Instance pipe(Instance carrier) 
-        throws IllegalStateException
-    {
+    public Instance pipe(Instance carrier) {
+
         CharSequence c = (CharSequence) carrier.getData();
         String[] pairs = c.toString().trim().split("\\s+");
-        String[] keys = new String[pairs.length];
+        int[] keys = new int[pairs.length];
         double[] values = new double[pairs.length];
 
         for (int i = 0; i < pairs.length; i++) {
-	  int delimIndex = pairs[i].lastIndexOf(":");
-	  if (delimIndex <= 0 || delimIndex == (pairs[i].length()-1))
-	      throw new IllegalStateException("token is not a valid feature name-feature value pair: "
-				        + pairs[i] + "\nfaulting instance name:" + carrier.getName());
-	  
-	  keys[i] = pairs[i].substring(0, delimIndex);
-	  values[i] = Double.parseDouble(pairs[i].substring(delimIndex+1));
+			int delimIndex = pairs[i].lastIndexOf(":");
+			if (delimIndex <= 0 || delimIndex == (pairs[i].length()-1)) {
+				keys[i] = dataAlphabet.lookupIndex(pairs[i], true);
+				values[i] = 1.0;
+			}
+			else {
+				keys[i] = dataAlphabet.lookupIndex(pairs[i].substring(0, delimIndex), true);
+				values[i] = Double.parseDouble(pairs[i].substring(delimIndex+1));
+			}
+        }
 
-	  dataAlphabet.lookupIndex(keys[i], true); // add the feature name
-        }
-        // Sort indices beforehand to prevent the bubble sort used in
-        // constructor of SparseVector from taking too much time
-        int[] keyIndices = FeatureVector.getObjectIndices(keys, dataAlphabet, true);
-        java.util.Arrays.sort(keyIndices);
-        FeatureVector fv = new FeatureVector(dataAlphabet, keyIndices, values);
-        // Check if we've set the target alphabet member
-        if (targetAlphabet == null) {
-	  if (carrier.getTarget() instanceof Labeling)
-	      targetAlphabet = ((Labeling)carrier.getTarget()).getLabelAlphabet();
-	  else
-	      throw new IllegalStateException ("Instance target is not a " +
-				         "Labeling; it is a " + 
-				         carrier.getTarget().getClass().getName());
-	  	  
-        }
-        
+		// [removed code that sorted indices but NOT values -DM]
+
+        FeatureVector fv = new FeatureVector(dataAlphabet, keys, values);
         carrier.setData( fv );
         return carrier;
     }
