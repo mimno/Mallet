d0035b6304cc676a18a856f0b82533696ca18ab1
David Mimno <david.mimno@gmail.com>
2014-05-02 15:27:14 -0400

    Fixed infinite loop, separated special cases

--- src/cc/mallet/types/Minkowski.java
+++ src/cc/mallet/types/Minkowski.java
@@ -10,125 +10,92 @@
  *  q=2. The special case with q equal to positive infinity is supported.
  *
  *  @author Jerod Weinman <A HREF="mailto:weinman@cs.umass.edu">weinman@cs.umass.edu</A>
-*/
+ */
 
 package cc.mallet.types;
 
-
-import java.lang.Math;
-
 import cc.mallet.types.SparseVector;
-
 
 public class Minkowski implements Metric {
 
     double q;
+	double oneOverQ;
 
     /** Constructor for Minkowski metric.
      *
      * @param q Power of component wise absolute difference; must be at least 1
      */
-    public Minkowski( double q )
-    {
-	if (q<1)
-	    throw new IllegalArgumentException("Argument q must be at least 1.");
-	
-	//assert( q>= 1 );
-	
-	this.q = q;
-
+    public Minkowski( double q ) {
+		this.q = q;
+		this.oneOverQ = 1.0 / q;
     }
+
+	public static Metric getMetric(double q) {
+		if (q == 1.0) { return new ManhattenDistance(); }
+		else if (q == 2.0) { return new EuclideanDistance(); }
+		else if (q == Double.POSITIVE_INFINITY) { return new InfiniteDistance(); }
+		else {
+			return new Minkowski(q);
+		}
+	}
     
     /**  Gives the Minkowski distance between two vectors.
      *
      *  distance(x,y) := \left( \Sum_i=0^d-1 \left| x_i - y_i \right|^q \right)^\frac{1}{q}
-     *  
-     *  for 1<=q<infinity. For q=infinity
-     *
-     *  distance(x,y) := max_i \left| x_i - y_i \right|
      */
-    public double distance( SparseVector a, SparseVector b)
-    {
-
-	double dist = 0;
-	double diff;
-	
-	if (a==null || b==null)
-	    throw new IllegalArgumentException("Distance from a null vector is undefined.");
-	
-	//assert (a != null);
-	//assert (b != null);
-
-	if (a.numLocations() != b.numLocations() )
-	    throw new IllegalArgumentException("Vectors must be of the same dimension.");
-
-	//assert (a.numLocations() == b.numLocations() );
-
-	for (int i=0 ; i< a.numLocations() ; i++ )
-	{
-	    diff = Math.abs( a.valueAtLocation(i) - b.valueAtLocation(i));
-
-	    if (q==1)
-		dist += diff;
-	    else if (q==2)
-		dist += diff*diff;
-	    else if (q==Double.POSITIVE_INFINITY)
-		if ( diff > dist)
-		    dist = diff;
-	    else
-		dist += Math.pow( diff, q );
-	    
-	}
-
-	if (q==1 || q==Double.POSITIVE_INFINITY)
-	    return dist;
-	else if (q==2)
-	    return Math.sqrt( dist );
-	else
-	    return Math.pow( dist, 1/q);
-    }
-
-	public double euclideanDistance(SparseVector a, SparseVector b)    {
+    public double distance( SparseVector a, SparseVector b) {
 		double dist = 0;
 		double diff;
 		
-		if (a==null || b==null)
+		if (a==null || b==null) {
 		    throw new IllegalArgumentException("Distance from a null vector is undefined.");
-		int aLen = a.numLocations();
-		int bLen = b.numLocations();
-		int ia = 0;
-		int ib = 0;
-		int indicea, indiceb;
-		while (ia < aLen && ib < bLen) {
-			indicea = a.indexAtLocation(ia);
-			indiceb = b.indexAtLocation(ib);
-			if(indicea < indiceb) {
-				diff = a.valueAtLocation(ia);
-				ia ++;
+		}
+
+		int leftLength = a.numLocations();
+		int rightLength = b.numLocations();
+		int leftIndex = 0;
+		int rightIndex = 0;
+		int leftFeature, rightFeature;
+
+		// We assume that features are sorted in ascending order.
+		// We'll walk through the two feature lists in order, checking
+		//  whether the two features are the same.
+
+		while (leftIndex < leftLength && rightIndex < rightLength) {
+
+			leftFeature = a.indexAtLocation(leftIndex);
+			rightFeature = b.indexAtLocation(rightIndex);
+
+			if (leftFeature < rightFeature) {
+				diff = Math.abs(a.valueAtLocation(leftIndex));
+				leftIndex ++;
+			}
+			else if (leftFeature == rightFeature) {
+				diff = Math.abs(a.valueAtLocation(leftIndex) - b.valueAtLocation(rightIndex));
+				leftIndex ++;
+				rightIndex ++;
 			}
 			else {
-				if(indicea == indiceb) {
-					diff = Math.abs(a.valueAtLocation(ia) - b.valueAtLocation(ib));
-					ia ++;
-					ib ++;
-				}
-				else
-				{
-					diff = b.valueAtLocation(ib);
-					ib ++;
-				}
+				diff = Math.abs(b.valueAtLocation(rightIndex));
+				rightIndex ++;
 			}
-			dist += diff * diff;
+
+			dist += Math.pow(diff, q);
 		}
-		while(ia < aLen) {
-			diff = a.valueAtLocation(ia);
-			dist += diff * diff;
+
+		// Pick up any additional features at the end of the two lists.
+		while (leftIndex < leftLength) {
+			diff = Math.abs(a.valueAtLocation(leftIndex));
+			dist += Math.pow(diff, q);
+			leftIndex++;
 		}
-		while(ib < bLen) {
-			diff = b.valueAtLocation(ib);
-			dist += diff * diff;
+
+		while (rightIndex < rightLength) {
+			diff = Math.abs(b.valueAtLocation(rightIndex));
+			dist += Math.pow(diff, q);
+			rightIndex++;
 		}
-		dist = Math.sqrt(dist);
-		return dist;
+
+		return Math.pow(dist, oneOverQ);
 	}
 }
