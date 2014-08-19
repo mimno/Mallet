91c7c726fdeba7ea5548adad8006e20fb19f9ca2
Alexandre Patry <alexandre.patry@nlpfu.com>
2014-08-18 21:07:30 -0400

    Override Classifier#print(PrintWriter) in MaxEnt

--- src/cc/mallet/classify/MaxEnt.java
+++ src/cc/mallet/classify/MaxEnt.java
@@ -203,8 +203,8 @@ public class MaxEnt extends Classifier implements Serializable
 		print(System.out);
 	}
 
-	public void print (PrintStream out)
-	{
+	@Override
+	public void print(PrintWriter out) {
 		final Alphabet dict = getAlphabet();
 		final LabelAlphabet labelDict = getLabelAlphabet();
 
@@ -221,6 +221,10 @@ public class MaxEnt extends Classifier implements Serializable
 				out.println (" "+name+" "+weight);
 			}
 		}
+	}
+
+	public void print (PrintStream out) {
+		print(new PrintWriter(out));
 	}
 
 	//printRank, added by Limin Yao
