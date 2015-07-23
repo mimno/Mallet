e4de34e3493f4e57d4e24e845379f1d6ff2f1b9f
mimno <mimno@cornell.edu>
2015-07-23 16:40:14 -0400

    moved assertion to address uninitialized value error

--- src/cc/mallet/types/CrossValidationIterator.java
+++ src/cc/mallet/types/CrossValidationIterator.java
@@ -50,8 +50,8 @@ implements java.util.Iterator<InstanceList[]>, Serializable {
      * @param r The source of randomness to use in shuffling.
      */
     public CrossValidationIterator (InstanceList ilist, int nfolds, java.util.Random r) {                       
-        assert (nfolds > 0) : "nfolds: " + this.nfolds;
         this.nfolds = nfolds;
+        assert (nfolds > 0) : "nfolds: " + this.nfolds;
         this.index = 0;
         double fraction = (double) 1 / nfolds;
         double[] proportions = new double[nfolds];
