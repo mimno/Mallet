690cfaa1c0991ccc711e86b12a0d9b0dd78a40d0
Cyrille Savelief <csavelief@mncc.fr>
2019-11-06 11:29:10 +0100

    [MALLET] Fix failing test testPrinting() in order to make it work with OS other than UNIX.

--- src/cc/mallet/pipe/tsf/tests/TestSequencePrintingPipe.java
+++ src/cc/mallet/pipe/tsf/tests/TestSequencePrintingPipe.java
@@ -6,15 +6,20 @@
    information, see the file `LICENSE' included with this distribution. */
 package cc.mallet.pipe.tsf.tests;
 
-import junit.framework.*;
-
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
 import cc.mallet.pipe.Pipe;
-import cc.mallet.pipe.SerialPipes;
 import cc.mallet.pipe.tsf.SequencePrintingPipe;
-import cc.mallet.types.*;
+import cc.mallet.types.Alphabet;
+import cc.mallet.types.FeatureVector;
+import cc.mallet.types.FeatureVectorSequence;
+import cc.mallet.types.Instance;
+import cc.mallet.types.LabelAlphabet;
+import cc.mallet.types.LabelSequence;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 /**
  * Created: Jul 8, 2005
@@ -72,7 +77,7 @@ public class TestSequencePrintingPipe extends TestCase {
             "\n" +
             "LABEL2 feature1\n" +
             "LABEL1 feature0\n\n",
-            sw.toString());
+            sw.toString().replaceAll("\\r\\n?", "\n"));
   }
 
   private static Alphabet dictOfSize (int n)
