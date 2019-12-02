006492e1f5da2d3cd468ed7c539001c4078259f9
Cyrille Savelief <csavelief@mncc.fr>
2019-12-02 10:07:28 +0100

    [CORE] Fix failing test TestMatrixOps.saveAndLoad() in order to make it work with locales other than english.

--- src/cc/mallet/types/MatrixOps.java
+++ src/cc/mallet/types/MatrixOps.java
@@ -13,9 +13,12 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.ArrayList;
 import java.util.Formatter;
 import java.util.List;
+import java.util.Locale;
 import java.util.Random;
 
 /** 
@@ -701,14 +704,17 @@ public final class MatrixOps {
     }
     
     public static void savetxt(double[][] matrix, File saveFile) throws Exception {
+
         PrintWriter out = new PrintWriter(Files.newBufferedWriter(saveFile.toPath(), Charset.defaultCharset()));
-        
+        DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols(Locale.ENGLISH);
+        DecimalFormat decimalFormat = new DecimalFormat ("0.####", decimalFormatSymbols);
+
         for (int row = 0; row < matrix.length; row++) {
             Formatter lineFormatter = new Formatter();
             for (int col = 0; col < matrix[row].length - 1; col++) {
-                lineFormatter.format("%f ", matrix[row][col]);
+                lineFormatter.format("%s ", decimalFormat.format(matrix[row][col]));
             }
-            lineFormatter.format("%f ", matrix[row][ matrix[row].length - 1 ]);
+            lineFormatter.format("%s ", decimalFormat.format(matrix[row][ matrix[row].length - 1 ]));
             out.println(lineFormatter);
         }
         
