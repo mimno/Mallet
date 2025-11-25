32171b5b381acb36b3822e14be7981c60ea1c287
Raghunath Mahakud <mail2raghunath298@gmail.com>
2025-11-25 19:04:16 +0530

    review commeents added

--- src/cc/mallet/util/tests/TestJShellCLI.java
+++ src/cc/mallet/util/tests/TestJShellCLI.java
@@ -1,9 +1,3 @@
-/* Copyright (C) 2003 Univ. of Massachusetts Amherst, Computer Science Dept.
-   This file is part of "MALLET" (MAchine Learning for LanguagE Toolkit).
-   http://www.cs.umass.edu/~mccallum/mallet
-   This software is provided under the terms of the Common Public License,
-   version 1.0, as published by http://www.opensource.org.  For further
-   information, see the file `LICENSE' included with this distribution. */
 package cc.mallet.util.tests;
 import junit.framework.TestCase;
 import jdk.jshell.JShell;
